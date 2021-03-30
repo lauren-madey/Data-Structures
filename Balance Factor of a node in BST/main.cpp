905521100036e285c2a0af10763e91382dc32bd7
Lauren Madey <elizabeth.lauren.441@gmail.com>
2021-03-29 21:03:57 -0400

    Update main.cpp

--- Balance Factor of a node in BST/main.cpp
+++ Balance Factor of a node in BST/main.cpp
@@ -1,3 +1,26 @@
+/*
+ZYBOOKS
+8.17 Calculate the balance factor of a node in a BST.
+Using the following definition for a BST node:
+
+class BTNode {
+public:
+  int data;
+  BTNode *left;
+  BTNode *right;
+  BTNode(int d,BTNode *l=nullptr,BTNode *r=nullptr)
+    :data(d),left(l),right(r)
+  {}
+};
+Implement a function to calculate the balance factor of a node in a BST. The function prototype must match the following function:
+
+int balance_factor(BTNode *subtree)
+{
+   // IMPLEMENT
+   return -2;
+}
+You may implement other functions to help you. In particular, you may want to implement a function to calculate a node's height.
+*/
 #include <iostream>
 #include <fstream>
 #include <random>
